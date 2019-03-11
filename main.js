fbac762c357637a6ff9181d2c41689387344b2c5
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-03-10 20:07:05 -0700

    modified to new api design

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 var _globalFile;
 var _api = {
-    domain : 'http://localhost:8080/ImageProcessor/api'
+    domain: 'http://localhost:8080/ImageProcessor/api'
 }
+let transformData = {
+    imageAsBase64String: null,
+    transformations: null
+}
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            let encoded = reader.result; //.replace("data:", "").replace(/^.*;base64,/, "");
+            // if ((encoded.length % 4) > 0) {
+            //     encoded += '='.repeat(4 - (encoded.length % 4));
+            // }
+            resolve(encoded);
+        };
+        reader.onerror = error => reject(error);
+    });
+}
+
 var uploadFile = function (files) {
     var files = document.getElementById("myfile").files;
     console.log(files);
@@ -10,72 +29,62 @@ var uploadFile = function (files) {
     }
     file = files[0];
     _globalFile = file;
-    var formData = new FormData();
-    formData.append("file", file);
-    $.ajax({
-        url: _api.domain + '/uploadImage',
-        type: 'POST',
-        data: formData,
-        success: function (data) {
-            console.log(data);
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $("#imageProcessor").css('display', 'inline');
-                $('#originalImage').attr('src', e.target.result);
-                $('#transformedImage').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(file);
-        },
-        cache: false,
-        contentType: false,
-        processData: false
+
+    getBase64(file).then(function (data) {
+        transformData.imageAsBase64String = data;
+        $("#imageProcessor").css("display", "block");
+        $("#originalImage").attr("src", data);
+        $("#transformedImage").attr("src", data);
     });
 };
 
-var grayscale = function () {
+function transform1() {
+    console.log($("#transformations_form"))
+}
+
+function transform() {
+    console.log(transformData);
     $.ajax({
-        url: _api.domain + '/transform/grayscale',
+        url: _api.domain + '/transform',
         type: 'POST',
-        data: null,
-        success: function (res) {
-            console.log(res);
-            console.log(_globalFile);
-            $('#transformedImage').attr('src', 'data:' + _globalFile.type + ';base64,' + res.data.success);
+        data: JSON.stringify(transformData),
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (data) {
+            console.log(data);
         },
         cache: false,
         contentType: false,
-        processData: false
+        processData: false,
+        contentType: "application/json;charset=UTF-8"
     });
 }
 
-var flipHorizontal = function () {
-    $.ajax({
-        url: _api.domain + '/transform/flip/horizontal',
-        type: 'POST',
-        data: null,
-        success: function (res) {
-            console.log(res);
-            $('#transformedImage').attr('src', 'data:' + _globalFile.type + ';base64,' + res.data.success);
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
+$(document).on('change', '#chkbx', function (e) {
+    console.log(e);
+    if (e.target.checked) {
+        toggle(e.target.value, "block");        
+    } else {
+        toggle(e.target.value, "none");    
+    }
+});
+
+function toggle(value, toggle) {
+    switch (value) {
+        case "FLIP":
+            console.log("flip");
+            $("#flip_params").css("display", toggle);
+            break;
+        case "ROTATE":
+            console.log("rotate")
+            $("#rotate_params").css("display", toggle);
+            break;
+        case "RESIZE":
+            console.log("resize")
+            $("#resize_params").css("display", toggle);
+            break;
+        default:
+            break;
+    }
 }
-
-var flipVertical = function () {
-    $.ajax({
-        url: _api.domain + '/transform/flip/vertical',
-        type: 'POST',
-        data: null,
-        success: function (res) {
-            console.log(res);
-            $('#transformedImage').attr('src', 'data:' + _globalFile.type + ';base64,' + res.data.success);
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-}
-
-
